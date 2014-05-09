5b82ddcf07d3f407490ceb73b04ab83f3bc19d04
Beth <embrown8@gmail.com>
2014-05-09 17:21:18 -0400

    Added css

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 var express = require('express'),
     mustache = require('mustachex'),
+    path = require('path'),
     app = express();
 
 app.engine('html', mustache.express);
 app.set('view engine', 'html');
 app.set('views', __dirname + '/views');
+app.use(express.static(path.join(__dirname, 'public')));
+
 
 app.get('/', function(req, res) {
   res.render('index',
